96fdd803b5c3fb07a1eadcce68ff5e9281d449d1
Mohamad <mohamadfarman977@gmail.com>
2021-05-06 21:20:15 +0200

    Started with structs and methods to handle exam details

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # exam-scheduler
 A small application which schedules studying preparations for an exam.
+
+### To-do
+- [] Make exam.ExamName() on line 48 output to stdout.
+
+### Intention
+#### User Input
+User should be able to specifiy name of exam they are preparing for and when it is.
+User should be able to specifiy the number of pages they need to go through.
+User should be able to specifiy the number of days they can afford not studying.
+User should be able to specifiy if they want to have days left for revision before the exam. & if so, how many.
+
+#### Computation
+Calculate the numbers of pages that needs to be read & understood on average per day.
+
+#### Output
+Present the name of the exam and number of pages that has to be done on average per day.
--- main.go
+++ main.go
@@ -1,8 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func WelcomeMessage() {
+	fmt.Println("This is an application that will help you schedule for your exams.")
+}
+
+type examOutput interface {
+	ExamName()
+	ExamDate()
+	ExamDetails()
+}
+
+type examDetails struct {
+	Name string
+	Date string
+}
+
+func (eD examDetails) UserInput() {
+	fmt.Println("What exam are you taking?")
+	fmt.Scanln(&eD.Name)
+	fmt.Println("When is your exam? (E.g 2017-05-14)")
+	fmt.Scanln(&eD.Date)
+}
+
+func (eD examDetails) ExamName() string {
+	return eD.Name
+}
+
+func (eD examDetails) ExamDate() string {
+	return eD.Date
+}
+
+func (eD examDetails) ExamDetails() (string, string) {
+	return eD.Name, eD.Date
+}
 
 func main() {
+	WelcomeMessage()
 
-	fmt.Println("vim-go")
+	var exam examDetails
+
+	exam.UserInput()
+
+	fmt.Println(exam.ExamName())
 }
