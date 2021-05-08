55e977045b386e1dd6e56c9c3d8b7ff82eb78d46
Mohamad <mohamadfarman977@gmail.com>
2021-05-08 19:13:56 +0200

    Fixed user input, modified ExamOutput interface & added logging functionality

--- README.md
+++ README.md
@@ -2,14 +2,17 @@
 A small application which schedules studying preparations for an exam.
 
 ### To-do
-- [] Make exam.ExamName() on line 48 output to stdout.
+- [x] Make exam.ExamName() on line 48 output to stdout.
 
-### Intention
+### Intention 
 #### User Input
-User should be able to specifiy name of exam they are preparing for and when it is.
-User should be able to specifiy the number of pages they need to go through.
-User should be able to specifiy the number of days they can afford not studying.
-User should be able to specifiy if they want to have days left for revision before the exam. & if so, how many.
+- [x] User should be able to specifiy name of exam they are preparing for and when it is.
+
+- [] User should be able to specifiy the number of pages they need to go through.
+
+- [] User should be able to specifiy the number of days they can afford not studying.
+
+- [] User should be able to specifiy if they want to have days left for revision before the exam. & if so, how many.
 
 #### Computation
 Calculate the numbers of pages that needs to be read & understood on average per day.
--- main.go
+++ main.go
@@ -2,48 +2,84 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	logrus "github.com/sirupsen/logrus"
 )
+
+func logger() {
+	// Initialize our logger from logrus
+
+	var filename string = "exam-scheduler.log"
+
+	// Will create a file if it doesn't exist, will append if it exists
+	f, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	Formatter := new(logrus.TextFormatter)
+	Formatter.TimestampFormat = "02-01-2006 15:04:05"
+	Formatter.FullTimestamp = true
+	logrus.SetFormatter(Formatter)
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		logrus.SetOutput(f)
+	}
+}
 
 func WelcomeMessage() {
 	fmt.Println("This is an application that will help you schedule for your exams.")
 }
 
-type examOutput interface {
-	ExamName()
-	ExamDate()
-	ExamDetails()
+type ExamOutput interface {
+	ExamName() string
+	ExamDate() string
+	ExamDetails() (string, string)
 }
 
-type examDetails struct {
+type ExamDetails struct {
 	Name string
 	Date string
 }
 
-func (eD examDetails) UserInput() {
-	fmt.Println("What exam are you taking?")
+func (eD *ExamDetails) UserInput() {
+	// Initializes variables in ExamDetails() from user
+
+	fmt.Println("Please enter the name of your exam: ")
 	fmt.Scanln(&eD.Name)
-	fmt.Println("When is your exam? (E.g 2017-05-14)")
+	fmt.Println("Please enter the date of the exam. (E.g. 2017-04-14): ")
 	fmt.Scanln(&eD.Date)
 }
 
-func (eD examDetails) ExamName() string {
+func (eD ExamDetails) ExamName() string {
+	// Returns name of the exam
+
 	return eD.Name
 }
 
-func (eD examDetails) ExamDate() string {
+func (eD ExamDetails) ExamDate() string {
+	// Returns the date of the exam
+
 	return eD.Date
 }
 
-func (eD examDetails) ExamDetails() (string, string) {
-	return eD.Name, eD.Date
+func (eD ExamDetails) ExamDetails() string {
+	// Returns all the information about the exam
+
+	s := eD.Name + " " + eD.Date
+	return s
 }
 
 func main() {
+	logger()
 	WelcomeMessage()
 
-	var exam examDetails
+	var exam ExamDetails
 
 	exam.UserInput()
 
-	fmt.Println(exam.ExamName())
+	fmt.Printf("Name of the exam is %s and the date is %s.\nHere is the full exam detail: %s", exam.ExamName(), exam.ExamDate(), exam.ExamDetails())
+
+	// logrus.Info("Name of the exam is %s and the date is %s.\nHere is the full exam detail: %s", exam.ExamName(), exam.ExamDate(), exam.ExamDetails())
+
+	//fmt.Printf("Name of exam is %s and the date is %s\n", exam.Name, exam.Date)
 }
